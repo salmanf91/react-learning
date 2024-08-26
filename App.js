f6895f2ab8e682c8ea8dc890b003fbcd237a718d
SF070036 <Salman.Farees@cerner.com>
2024-08-26 18:12:53 +0530

    ep 2 laying the foundation

--- App.js
+++ App.js
@@ -1,19 +1,51 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement("div", {id: "parent"}, [
-    React.createElement("div", {id: "child1"}, [
-        React.createElement("h1", {}, "I am a Namaste React"),
-        React.createElement("h2", {}, "I am a h2 tag")
-    ]),
-    React.createElement("div", {id: "child2"}, [
-        React.createElement("h1", {}, "I am a h1 tag"),
-        React.createElement("h2", {}, "I am a h2 tag")
-    ]),
-])
+//React element
 
-console.log(parent);
+// React.createElement => React Element (JS Object) => HTML Element (render)
+
+//create reactElement using core react
+const heading = React.createElement("h1", {id: "heading"}, "React Learning");
+
+console.log(heading)
+
+// JSX => transpiles it to React.createElement =>  React Element (JS Object) => HTML Element (render)
+
+//create reactElement using JSX syntax. It is same as above sentence
+// const jsxHeading = <h1 id="heading">React with JSX</h1>
+
+const jsxHeading = (<h1 id="heading">
+    React with JSX
+    </h1>)
+
+
+//React Component
+const HeadingComponent = () => {
+    return <h1>This is a react functional component</h1>;
+}
+
+const ShortHandComponent = () => <h1>This is a shorthand functional component.</h1>
+
+const number = 1000;
+
+const AnotherWayToWriteComponent = () => (
+    <div id="container">
+        <HeadingComponent />
+        <NormalFunctionForReactExample />
+        <h3>This also a react functionl component</h3>
+        <h4>{number}</h4>
+        <div>{jsxHeading}</div>
+    </div>
+)
+
+const NormalFunctionForReactExample = function () {
+	return <h2>Normal function example </h2>;
+} 
+
+console.log(jsxHeading)
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
-root.render(parent);
+// root.render(jsxHeading);
+root.render(<AnotherWayToWriteComponent />);
